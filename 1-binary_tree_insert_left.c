cdb427d111c6dc4b3dfe079facee2bf729ab9858
wadenya <alexwadenya001@gmail.com>
2023-08-01 06:32:55 -0700

    test

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -13,19 +13,27 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	binary_tree_t *new_node;
 
 	if (parent == NULL)
+	{
 		return (NULL);
+	}
 
 	new_node = malloc(sizeof(binary_tree_t));
-	if (!new_node)
+	if (new_node == NULL)
+	{
 		return (NULL);
+	}
 
 	new_node->n = value;
 	new_node->parent = parent;
 	new_node->left = parent->left;
 	new_node->right = NULL;
 
-	if (new_node->left)
+	if (new_node->left != NULL)
+	{
 		new_node->left->parent = new_node;
-	return (new_node);
+	}
 
+	parent->left = new_node;
+
+	return (new_node);
 }
